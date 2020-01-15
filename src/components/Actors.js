31a98883be38267670c28056efcca7891e0ad5a3
dawncronin <github email address>
2020-01-15 09:46:33 -0800

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,8 +4,18 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
-    </div>
+    <h1>Actors Page</h1>
+    {actors.map(actor => {
+      return (
+        <div> 
+          <h2>{actor.name}</h2>
+          <ul>
+            {actor.movies.map(movie => <li>{movie}</li>)}
+          </ul>
+        </div>
+      )
+    })}
+  </div>
   );
 };
 
--- src/data.js
+++ src/data.js
@@ -55,4 +55,4 @@ module.exports = {
   movies,
   actors,
   directors
-}
+};
