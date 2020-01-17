6f88804eb5db0c861ffea5a6ceaddfb4c4797f98
mwong068 <mwong068@gmail.com>
2020-01-17 11:19:36 -0800

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,38 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        <div>
+          <h2>Doctor Strange</h2>
+          <h3>115</h3>
+          <ul>
+            <li>Action</li>
+            <li>Adventure</li>
+            <li>Fantasy</li>
+          </ul>
+        </div>
+        <div>
+          <h2>Trolls</h2>
+          <h3>92</h3>
+          <ul>
+            <li>Animation</li>
+            <li>Adventure</li>
+            <li>Family</li>
+            <li>Comedy</li>
+            <li>Fantasy</li>
+          </ul>
+        </div>
+        <div>
+          <h2>Jack Reacher: Never Go Back</h2>
+          <h3>118</h3>
+          <ul>
+            <li>Adventure</li>
+            <li>Action</li>
+            <li>Crime</li>
+            <li>Mystery</li>
+            <li>Thriller</li>
+          </ul>
+        </div>
     </div>
   );
 };
